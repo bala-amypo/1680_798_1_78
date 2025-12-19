fcdc5ed0628c84d9a61d7bb880e2c66532e5aab0
bala-amypo <bala@amypo.in>
2025-12-19 09:56:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/TaskAssignmentController.java
+++ src/main/java/com/example/demo/controller/TaskAssignmentController.java
@@ -1,21 +1,26 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.service.TaskAssignmentService;
 import com.example.demo.model.TaskAssignmentRecord;
-import com.example.demo.service.impl.TaskAssignmentServiceImpl;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/assignments")
+@Tag(name = "Task Assignments")
 public class TaskAssignmentController {
+private final TaskAssignmentService service;
 
-    private final TaskAssignmentServiceImpl service;
 
-    public TaskAssignmentController(TaskAssignmentServiceImpl service) {
-        this.service = service;
-    }
+public TaskAssignmentController(TaskAssignmentService service) {
+this.service = service;
+}
 
-    @PostMapping("/assign/{taskId}")
-    public TaskAssignmentRecord assign(@PathVariable Long taskId) {
-        return service.assignTask(taskId);
-    }
+
+@PostMapping("/assign/{taskId}")
+public TaskAssignmentRecord assign(@PathVariable Long taskId) {
+return service.assignTask(taskId);
+}
 }
