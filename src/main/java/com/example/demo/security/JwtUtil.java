47d3d283b1f7665b064a8b91be80a7ae12bfc83d
bala-amypo <bala@amypo.in>
2025-12-22 08:52:28 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,20 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "MySecretKey12345"; // Use env variable in production
+    private final long EXPIRATION = 1000 * 60 * 60 * 10; // 10 hours
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
