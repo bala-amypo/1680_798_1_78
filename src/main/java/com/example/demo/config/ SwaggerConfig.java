8b02e9cc8f26d98497d5ae57db76491b04d3bd9b
bala-amypo <bala@amypo.in>
2025-12-22 10:26:27 +0000

    User commit

--- src/main/java/com/example/demo/config/ SwaggerConfig.java
+++ src/main/java/com/example/demo/config/ SwaggerConfig.java
@@ -1,20 +1,13 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.security.SecuritySchemeType;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.security.SecuritySchemeType;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "Volunteer Task Management API", version = "1.0"),
-        security = @SecurityRequirement(name = "bearerAuth")
-)
+@OpenAPIDefinition(info = @Info(title = "Volunteer API", version = "1.0"))
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
@@ -22,5 +15,4 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class SwaggerConfig {
-    // Swagger/OpenAPI configuration with JWT Bearer support
 }
