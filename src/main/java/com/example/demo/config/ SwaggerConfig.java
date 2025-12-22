fc77c7e9def90cb0f0a49a2c5774e8c983107ad9
bala-amypo <bala@amypo.in>
2025-12-22 09:10:33 +0000

    User commit

--- src/main/java/com/example/demo/config/ SwaggerConfig.java
+++ src/main/java/com/example/demo/config/ SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemeType;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
