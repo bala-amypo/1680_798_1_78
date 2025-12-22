a7f4c3354830dadb81373aab66e605d7e1289eb5
bala-amypo <bala@amypo.in>
2025-12-22 10:23:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.example.demo.service;
 import com.example.demo.model.User;
 
 public interface UserService {
-    User saveUser(User user);
     User findByEmail(String email);
-    boolean existsByEmail(String email);
+    User save(User user);
 }
