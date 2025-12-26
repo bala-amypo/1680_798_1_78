8d0347c361b64b7a98c3ed9acd86534f6441422a
bala-amypo <bala@amypo.in>
2025-12-26 04:52:27 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,19 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkillBasedVolunteerTaskAssignorApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SkillBasedVolunteerTaskAssignorApplication.class, args);
-    }
-}
-
-package com.example.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
