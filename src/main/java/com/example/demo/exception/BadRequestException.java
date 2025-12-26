c432de825bd7757bba77db480c4a7aa241a04172
bala-amypo <bala@amypo.in>
2025-12-26 04:15:43 +0000

    User commit

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -1,8 +1,29 @@
 package com.example.demo.exception;
 
+/**
+ * Custom exception used across services
+ * to indicate invalid input or illegal operations.
+ *
+ * This is explicitly asserted in TestNG test cases.
+ */
 public class BadRequestException extends RuntimeException {
 
     public BadRequestException(String message) {
         super(message);
     }
 }
+
+
+
+
+
+
+
+// package com.example.demo.exception;
+
+// public class BadRequestException extends RuntimeException {
+
+//     public BadRequestException(String message) {
+//         super(message);
+//     }
+// }
